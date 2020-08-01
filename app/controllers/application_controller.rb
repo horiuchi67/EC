69ba11a351b6a85fd711ff38a57fc075fd4b9610
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-01 20:41:32 +0900

    adminとcustomerのdevisewo 作りました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      root_path
+    when Admins
+      admin_customers_path
+    end
+  end
+  def after_sign_out_path_for(resource)
+    case resource
+    when :customer
+      root_path
+    when :admin
+      new_admin_session_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+    namespace :admin do
+  	get '/' => "home#top"
+  	resources :products,only: [:index,:new,:create,:show,:edit,:update]
+  	resources :genres,only: [:index,:create,:edit,:update]
+  	resources :customers,only: [:index,:show,:edit,:update]
+  	resources :orders,only: [:index,:show,:update]
+  	resources :order_details,only: [:update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
